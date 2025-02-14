f4918eccb30d57cbd3c66e4b4d24f7c11b44b0e7
coramdeo59 <melkamuelias59@gmail.com>
2025-02-14 17:28:40 +0300

    cookie implementation

--- src/iam/authentication/authentication.controller.ts
+++ src/iam/authentication/authentication.controller.ts
@@ -1,4 +1,5 @@
-import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post, Res } from '@nestjs/common';
+import { Response } from 'express';
 import { SignUpDto } from './dto/sign-up.dto/sign-up.dto';
 import { AuthenticationService } from './authentication.service';
 import { SignInDto } from './dto/sign-in.dto/sign-in.dto';
@@ -17,8 +18,17 @@ export class AuthenticationController {
 
     @HttpCode(HttpStatus.OK)
     @Post('sign-in')
-    signIn(@Body() SignInDto: SignInDto) {
-        return this.authService.signIn(SignInDto)
+    async signIn(
+        @Res({ passthrough: true }) response: Response, 
+        @Body() signInDto: SignInDto 
+    ) {
+        const accessToken = await this.authService.signIn(signInDto);
+        response.cookie('accessToken', accessToken, {
+            secure: true,
+            httpOnly: true,
+            sameSite: 'lax',
+        });
+        return { message: 'Signed in successfully' }; 
     }
 
 }
